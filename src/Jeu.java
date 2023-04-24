af4f692adcd19772feb0c99e3c01d5c3123e0fe2
juliechrt <mailchartier.julie@gmail.com>
2023-04-24 16:31:14 +0200

    feat: add Jeu & Partie classes

---
+++ src/Jeu.java
@@ -0,0 +1,17 @@
+/**
+ * La classe Jeu représente le programme principal du jeu de stratégie
+ *
+ * @author Julie Chartier & Paul Mestack
+ * @version 0.0.1
+ */
+public class Jeu {
+    /**
+     * La méthode main lance le jeu de stratégie.
+     *
+     * @param args les arguments de la ligne de commande
+     */
+    public static void main(String[] args) {
+        System.out.println("Bienvenue sur Jeu de Stratèges !");
+        Partie partie = new Partie();
+    }
+}
